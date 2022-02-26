6166ec0aabe9171ffceda1d7ed3e6d5b43241afb
Sujal Mandal <ss6sujal@gmail.com>
2022-02-26 16:10:16 +0400

    create dynmaic request

--- src/components/state-view-components/state-view/StateViewComponent.jsx
+++ src/components/state-view-components/state-view/StateViewComponent.jsx
@@ -5,29 +5,45 @@ import {useState} from "react";
 const StateComponent = function(){
 
     const [isStartState, setStartState] = useState(true);
-    const [currentForm, setCurrentForm] = useState([]);
-
-    const getNextState = function(){
-        axios.post("http://localhost:8080/state/process",{}).then(res => {
-            setCurrentForm(res.data.formToFill.elements);
+    const [data, setData] = useState(null);
+    const [request, setRequest] = useState({});
+ 
+    const getNextState = function(nextAction){
+        let reqBody={};
+        if(nextAction){
+            reqBody = {
+                "request": request,
+                "fromState": data?data.currentStep:"",
+                "action": nextAction?nextAction:"",
+                "sequence" : data.sequence
+            }; 
+        }
+        axios.post("http://localhost:8080/state/process", reqBody).then(res => {
+            setData(res.data);
             setStartState(false);
         });
     }
 
-
-    const currentStateTemplate = <button onClick={getNextState}>Start</button>;
-
     return (
     <div className='state-view' id="state-view">
-        {currentForm.map(element=>{
-            console.log(element)
-            return (
+        {data?data.formToFill.elements.map((element)=> 
             <div key={'div_'+element.id}>
-                <input id={element.id} name={element.name} type={element.type} placeholder={element.label}></input><br/>
+                <input id={element.id} name={element.name} type={element.type} placeholder={element.label} 
+                onChange={(e)=>{
+                    setRequest(prevState=> ({...prevState,[e.target.name]:e.target.value}));
+                }}></input><br/>
             </div>
-            )
-        })}
-        {isStartState==true?currentStateTemplate:""}
+        ):""}
+        {
+            isStartState?
+            <>
+                <button onClick={()=>getNextState()}>Start</button>
+            </>:
+            <>
+                <>{data.nextAction?<button onClick={()=>getNextState(data.nextAction)}>Next</button>:""}</>
+                <>{data.backAction?<button onClick={()=>getNextState(data.nextAction)}>Back</button>:""}</>
+            </>
+        }
     </div>
     );
 
